0554c91d49bc9766d3a148cb8c0aa3e20dd1ac29
rahmawatirhmn20 <61910706+strahmawatirahman@users.noreply.github.com>
2020-06-26 19:52:53 -0700

    Add files via upload

---
+++ KoneksiDataBase.java
@@ -0,0 +1,23 @@
+package oop11;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class KoneksiDataBase {
+		
+	public class koneksiDB {
+		private static Connection mysqlconfig;
+		public static Connection configDB()throws SQLException{
+			try {
+				String url="jdbc:mysql://localhost:3306/mahasiswa"; //url database
+				String user="root"; //user database
+				String pass=""; //password database
+				DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+				mysqlconfig=DriverManager.getConnection(url, user, pass);
+			} catch (SQLException e) {
+				System.err.println("koneksi gagal "+e.getMessage()); //perintah menampilkan error pada koneksi
+			}
+			return mysqlconfig;
+		}	
+	}
+}
